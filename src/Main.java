145f0b862420a31c3a7d4bc6b4d52ae5f5bb8795
FranDiana <din3711din@gmail.com>
2022-10-02 15:37:23 +0300

    dff

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int num[] = {-21, 22, 13, 4, 45, 63, 4, -8};
+        int max_neparne = num[0];
+        for (int i : num) {
+            if (i % 2 != 0) {
+                if (max_neparne < i) {
+                    max_neparne = i;
+                }
+            }
+        }
+        System.out.println("Max number = " + max_neparne);
+
     }
 }
